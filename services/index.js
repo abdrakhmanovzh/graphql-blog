e94afab2985ccc63141d3c64c20e9a20daf43224
abdrakhmanovzh <abdrakhmanovzh@gmail.com>
2023-02-17 21:33:59 +0600

    home page is finished

--- services/index.js
+++ services/index.js
@@ -4,7 +4,7 @@ const graphqlApi = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
     const query = gql`
-        query Assets {
+        query GetPosts {
             posts {
                 author {
                     bio
@@ -31,4 +31,60 @@ export const getPosts = async () => {
     const res = await request(graphqlApi, query);
 
     return res.posts;
+};
+
+export const getRecentPosts = async () => {
+    const query = gql`
+        query GetPostDetails {
+            posts(orderBy: createdAt_ASC, last: 3) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `;
+    const res = await request(graphqlApi, query);
+
+    return res.posts;
+};
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+        query GetPostDetails($slug: String!, $categories: [String]!) {
+            posts(
+                where: {
+                    slug_not: $slug
+                    AND: { categories_some: { slug_in: $categories } }
+                }
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `;
+    const res = await request(graphqlApi, query);
+
+    return res.posts;
+};
+
+export const getCategories = async () => {
+    const query = gql`
+        query GetCategories {
+            categories {
+                name
+                slug
+            }
+        }
+    `;
+    const res = await request(graphqlApi, query);
+
+    return res.categories;
 };
